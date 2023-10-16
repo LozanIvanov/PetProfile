6a16ade23de9b03f7232b35b7ab7125ffa15e526
LozanIvanov <Lozan.Ivanov@abv.bg>
2023-10-16 16:47:54 +0300

    BrotherandSister Controller and View, Parents controller and View, Home Page

---
+++ Pets/Controllers/BrothersAndSistersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pets.Controllers
+{
+    [Route("/BrothersAndSisters")]
+    public class BrothersAndSistersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/BrothersAndSisters/Index.cshtml");
+        }
+    }
+}
---
+++ Pets/Controllers/ParentsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pets.Controllers
+{
+    [Route("/Parents")]
+    public class ParentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Parents/Index.cshtml");
+        }
+    }
+}
